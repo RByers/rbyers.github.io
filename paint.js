7a7f55b283ee05e98f2e81d17f51ff00b0b5f129
mustaqahmed <mustaq@chromium.org>
2014-08-20 16:50:44 -0400

    Added radiusX, radiusY and rotation angle.
    See https://code.google.com/p/chromium/issues/detail?id=381394

--- paint.js
+++ paint.js
@@ -7,12 +7,14 @@ var nextCount = 0;
 var touchMap = {};
 var pointMode = (window.location.hash == "#points");
 var enableForce = false;
+var drawTouchAxes = true;
+var scale = 1;
 
-document.addEventListener('keyup', function(e) {
+document.addEventListener("keyup", function(e) {
   switch(e.which) {
     // ESC
     case 27:
-    var canvas = document.getElementById('canvas');
+    var canvas = document.getElementById("canvas");
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
     break;
 
@@ -27,6 +29,11 @@ document.addEventListener('keyup', function(e) {
     enableForce = !enableForce;
     break;
 
+    // a
+    case 65:
+    drawTouchAxes = !drawTouchAxes;
+    break;
+
     // enter
     case 13:
     if (document.documentElement.webkitRequestFullscreen) {
@@ -39,7 +46,7 @@ document.addEventListener('keyup', function(e) {
 
 });
 
-window.addEventListener('resize', function(e) {
+window.addEventListener("resize", function(e) {
     InitializeCanvas();
 });
 
@@ -56,65 +63,101 @@ function drawTouches(touches, eventType) {
     }
 
     // Polyfill non-standard properties
-    if (!('radiusX' in touch) && 'webkitRadiusX' in touch)
+    if (!("radiusX" in touch) && "webkitRadiusX" in touch)
       touch.radiusX = touch.webkitRadiusX;
-    if (!('force' in touch) && 'webkitForce' in touch)
+    if (!("radiusY" in touch) && "webkitRadiusY" in touch)
+      touch.radiusY = touch.webkitRadiusY;
+    if (!("rotationAngle" in touch) && "webkitRotationAngle" in touch)
+      touch.rotationAngle = touch.webkitRotationAngle;
+    if (!("force" in touch) && "webkitForce" in touch)
       touch.force = touch.webkitForce;
 
+    var radiusX = getAdjustedRadius(touch.radiusX);
+    var radiusY = getAdjustedRadius(touch.radiusY);
+    var rotationAngle = getAdjustedRotationAngle(touch.rotationAngle);
+
+    // Try to avoid start/end ellipses overlapping exactly
+    if (eventType == "touchend") {
+      radiusX++;
+      radiusY++;
+    }
+
+    context.save();
+    context.translate(touch.pageX * scale, touch.pageY * scale);
+    context.rotate(rotationAngle);
+    context.scale(radiusY/radiusX, 1);
     context.beginPath();
-
-    // Spec says to use 1 for unknown radius, can't differentiate between that
-    // and real 1 pixel radius.
-    if (touch.radiusX > 1)
-      radiusSupported = true;
-    var radius = radiusSupported ? touch.radiusX : 15;
-    if (radius > 100) {
-      console.error('Got large radiusX: ' + touch.radiusX);
-      radius=100;
-    }
-    if (pointMode)
-      radius=1/scale;
-
-    // Try to avoid start/end circles overlapping exactly
-    if (eventType == 'touchend') {
-      radius++;
-    }
- 
-    context.arc(touch.pageX * scale, touch.pageY * scale, radius * scale, 0, 2.0 * 3.14159, false);
+    context.arc(0, 0, radiusX, 0, 2.0 * Math.PI, false);
     context.closePath();
 
-    // Fill circle on start/move
-    if (eventType != 'touchend') {
+    // Fill the ellipse on start/move
+    if (eventType != "touchend") {
       var opacity = pointMode ? 1 : 0.1;
 
       var hue = (touchMap[touch.identifier] * 30) % 256;
       var lum = 40;
       if (enableForce && touch.force)
         lum = Math.round(touch.force / 0.4 * 50 + 20);
-      context.fillStyle = 'hsla(' + hue + ',100%,' + lum + '%, ' + opacity + ')';
+      context.fillStyle = "hsla(" + hue + ",100%," + lum + "%, " + opacity + ")";
       context.fill();
     }
 
-    // Outline circle on start/end
-    if (eventType != 'touchmove') {
-      context.strokeStyle = eventType == 'touchstart' ? 'black' : 'grey';
+    // Outline ellipse on start/end
+    if (eventType != "touchmove") {
+      context.strokeStyle = eventType == "touchstart" ? "black" : "grey";
       context.lineWidth = 2;
       context.stroke();
     }
 
+    if (drawTouchAxes && Math.abs(rotationAngle) > 0.01) {
+      context.strokeStyle = "grey";
+      context.lineWidth = 1;
+
+      context.beginPath();
+      context.moveTo(-radiusX, 0);
+      context.lineTo(radiusX, 0);
+      context.stroke();
+
+      context.beginPath();
+      context.moveTo(0, -radiusX);
+      context.lineTo(0, radiusX);
+      context.stroke();
+    }
+
+    context.restore();
   }
 }
 
+function getAdjustedRadius(radius) {
+  if (pointMode)
+    return 1;
+
+  // Spec says to use 1 for unknown radius, can't differentiate between that
+  // and real 1 pixel radius.
+  if (!radius || radius <= 1) {
+    radius = 15;
+  } else if (radius > 100) {
+    console.error("Got large radius: " + radius);
+    radius = 100;
+  }
+  
+  return radius * scale;
+}
+
+function getAdjustedRotationAngle(angle) {
+  return angle * Math.PI / 180;
+}
+
 function TouchHandler(event) {
-  drawTouches(event.changedTouches,event.type);
   event.preventDefault();
+  drawTouches(event.changedTouches,event.type);
 }
 
 var mousePressed = false;
 
 function MouseHandler(event) {
-  if (event.type == "mousedown" && event.button == 0) mousePressed = true;
-  if (mousePressed && event.button == 0)
+  if (event.type == "mousedown" && event.button === 0) mousePressed = true;
+  if (mousePressed && event.button === 0)
   {
     var fakeTouch = {
       identifier: 10,
@@ -130,10 +173,8 @@ function MouseHandler(event) {
   }
 }
 
-var scale = 1;
-
 function InitializeCanvas() {
-  var elem = document.getElementById('canvas');
+  var elem = document.getElementById("canvas");
   
   var newscale = window.devicePixelRatio ? window.devicePixelRatio : 1;
   var newwidth = window.screen.width * newscale;
@@ -144,10 +185,7 @@ function InitializeCanvas() {
     scale = newscale;
     elem.width = newwidth;
     elem.height = newheight;
-    elem.style.width = window.screen.width + 'px';
-    elem.style.height = window.screen.height + 'px';
+    elem.style.width = window.screen.width + "px";
+    elem.style.height = window.screen.height + "px";
   }
-
-  var context = GetContext();
-  //context.fillStyle = "#ff0000";
 }
