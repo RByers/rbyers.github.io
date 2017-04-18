f91dd30580a75b0af9fed4a2b9a2cb68c8b86072
Navid Zolghadr <nzolghadr@chromium.org>
2017-04-18 11:37:50 -0400

    Add coalesced events to eventRate example
    Add coalesced events to the drawing if the
    user agent supports pointerevents and
    getCoalescedEvents API is supported.

--- eventRate.js
+++ eventRate.js
@@ -1,4 +1,5 @@
 var points = [];
+var coalescedPoints = [];
 var eventCount = 0;
 var startTime;
 var canvas;
@@ -12,16 +13,19 @@ window.addEventListener('resize', function(e) {
     InitializeCanvas();
 });
 
-function drawPoints(points) {
+function drawPoints(points, isCoalesced) {
   var context = canvas.getContext('2d');
   for (var i = 0; i < points.length; ++i) {
+    var radius = isCoalesced ? 2.0 : 4.0;
 
     context.beginPath();
 
-    context.arc(points[i].x * scale, points[i].y * scale, 4 * scale, 0, 2.0 * 3.14159, false);
+    context.arc(points[i].x * scale, points[i].y * scale, radius * scale, 0, 2 * 3.14159, false);
     context.closePath();
-
-    context.fillStyle = 'rgba(0,0,100,0.5)';
+    if (isCoalesced)
+      context.fillStyle = 'rgba(100,0,0,0.5)';
+    else
+      context.fillStyle = 'rgba(0,0,100,0.5)';
     context.fill();
   }
 }
@@ -30,8 +34,10 @@ function onFrame()
 {
   frameCounter++;
   if (!startTime || frameCounter % parseInt(document.getElementById('throttle').value) == 0) {
-    drawPoints(points);
+    drawPoints(points, false);
+    drawPoints(coalescedPoints, true);
     points = [];
+    coalescedPoints = [];
 
     var extraWork = parseInt(document.getElementById('work').value);
     if (extraWork > 0) {
@@ -50,6 +56,7 @@ function startDraw()
 {
   startTime = performance.now();
   eventCount = 0;
+  coalescedEventCount = 0;
   frameCounter = 0;
   canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
   window.requestAnimationFrame(onFrame);
@@ -63,56 +70,92 @@ function endDraw()
   startTime = undefined;
 }
 
-function addPoint(x, y)
+function addCoalescedPoint(x, y)
 {
   eventCount++;
+  coalescedPoints.push({x:x, y:y});
+}
+
+function addPoint(x, y)
+{
+  coalescedEventCount++;
   points.push({x:x, y:y});
 }
 
-
 window.onload = function() {
   canvas = document.getElementById('canvas');
-  var mouseDown = false;
+  if (window.PointerEvent) {
+    var primaryButtonDown = false;
 
-  canvas.addEventListener('mousedown', function(e) {
-    if (e.button == 0) {
-      mouseDown = true;
-      startDraw();
-      addPoint(e.pageX, e.pageY);
+    canvas.addEventListener('pointerdown', function(e) {
+      if (e.button == 0) {
+        primaryButtonDown = true;
+        startDraw();
+        addPoint(e.pageX, e.pageY);
+        e.preventDefault();
+      }
+    });
+    canvas.addEventListener('pointermove', function(e) {
+      if (primaryButtonDown) {
+        if (e.getCoalescedEvents) {
+          e.getCoalescedEvents().forEach(function(ce) {
+            addCoalescedPoint(ce.pageX, ce.pageY);
+          });
+        }
+        addPoint(e.pageX, e.pageY);
+        e.preventDefault();
+      }
+    });
+    canvas.addEventListener('pointerup', function(e) {
+      if (e.button == 0) {
+        endDraw();
+        primaryButtonDown = false;
+        e.preventDefault();
+      }
+    });
+  } else {
+    var mouseDown = false;
+
+    canvas.addEventListener('mousedown', function(e) {
+      if (e.button == 0) {
+        mouseDown = true;
+        startDraw();
+        addPoint(e.pageX, e.pageY);
+        e.preventDefault();
+      }
+    });
+    canvas.addEventListener('mouseup', function(e) {
+      if (e.button == 0) {
+        endDraw();
+        mouseDown = false;
+        e.preventDefault();
+      }
+    });
+    canvas.addEventListener('mousemove', function(e) {
+      if (mouseDown) {
+        addPoint(e.pageX, e.pageY);
+        e.preventDefault();
+      }
+    });
+    canvas.addEventListener('touchstart', function(e) {
+      if (e.touches.length == e.changedTouches.length) {
+        startDraw();
+        addPoint(e.touches[0].pageX, e.touches[0].pageY);
+      }
       e.preventDefault();
-    }
-  });
-  canvas.addEventListener('mouseup', function(e) {
-    if (e.button == 0) {
-      endDraw();
-      mouseDown = false;
+    });
+    canvas.addEventListener('touchend', function(e) {
+      if (e.touches.length == 0) {
+        endDraw();
+      }
       e.preventDefault();
-    }
-  });
-  canvas.addEventListener('mousemove', function(e) {
-    if (mouseDown) {
-      addPoint(e.pageX, e.pageY);
+    });
+    canvas.addEventListener('touchmove', function(e) {
+      var touch = e.touches[0];
+      addPoint(touch.pageX, touch.pageY);
       e.preventDefault();
-    }
-  });
-  canvas.addEventListener('touchstart', function(e) {
-    if (e.touches.length == e.changedTouches.length) {
-      startDraw();
-      addPoint(e.touches[0].pageX, e.touches[0].pageY);
-    }
-    e.preventDefault();
-  });
-  canvas.addEventListener('touchend', function(e) {
-    if (e.touches.length == 0) {
-      endDraw();
-    }
-    e.preventDefault();
-  });
-  canvas.addEventListener('touchmove', function(e) {
-    var touch = e.touches[0];
-    addPoint(touch.pageX, touch.pageY);
-    e.preventDefault();
-  });
+    });
+  }
   InitializeCanvas();
 }
 
